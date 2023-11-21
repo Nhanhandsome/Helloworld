e4b7157034c9b9de6aeed93dbe1dac21d0371ed5
MinhNhan <nhanpm@selex.vn>
2023-11-21 13:51:49 +0700

    init

---
+++ Debug/src/Helloworld.d
@@ -0,0 +1,1 @@
+src/Helloworld.o: ../src/Helloworld.cpp
---
+++ src/Helloworld.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : Helloworld.cpp
+// Author      : MinhNhan
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello Guy!!" << endl; // prints !!!Hello Guy!!
+	return 0;
+}
